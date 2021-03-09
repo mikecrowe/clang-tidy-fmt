c5c66cf11dc515b41cf290f281902e369474c34b
Mike Crowe <mac@mcrowe.com>
2021-03-09 08:37:59 +0000

    Add FormatStringConverter and test stubs

---
+++ clang-tools-extra/clang-tidy/utils/FormatStringConverter.cpp
@@ -0,0 +1,22 @@
+//===---------- FormatStringConverter.cpp - clang-tidy --------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "FormatStringConverter.h"
+
+namespace clang {
+namespace tidy {
+namespace utils {
+
+std::string stdFormatStringFromPrintfFormatString(const std::string &PrintfFormat)
+{
+    return {};
+}
+
+} // namespace utils
+} // namespace tidy
+} // namespace clang
