2c0c649758520fb21367506b9c86afe9564e4d22
Mike Crowe <mac@mcrowe.com>
2021-03-16 08:16:15 +0000

    FormatStringConverter: Add first tests

--- clang-tools-extra/unittests/clang-tidy/FormatStringConverterTest.cpp
+++ clang-tools-extra/unittests/clang-tidy/FormatStringConverterTest.cpp
@@ -1,3 +1,24 @@
 #include "../clang-tidy/utils/FormatStringConverter.h"
 
-TEST(FormatStringConverter, EmptyString) {}
+#include "ClangTidy.h"
+#include "ClangTidyTest.h"
+#include "gtest/gtest.h"
+
+namespace clang {
+namespace tidy {
+namespace test {
+
+using clang::tidy::utils::stdFormatStringFromPrintfFormatString;
+
+TEST(FormatStringConverter, EmptyString) {
+  ASSERT_EQ(stdFormatStringFromPrintfFormatString(""), "");
+}
+
+TEST(FormatStringConverter, PlainString) {
+  const char plain[] = "This string contains nothing that needs translating";
+  ASSERT_EQ(stdFormatStringFromPrintfFormatString(plain), plain);
+}
+
+} // namespace test
+} // namespace tidy
+} // namespace clang
