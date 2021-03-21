964e628dcc65590d64b2caed135b1008bdf559b2
Mike Crowe <mac@mcrowe.com>
2021-03-21 18:03:18 +0000

    fmt-printf-convert: simple positional arguments and widths work!

--- clang-tools-extra/test/clang-tidy/checkers/fmt-printf-convert.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/fmt-printf-convert.cpp
@@ -13,3 +13,29 @@ void printf_string() {
   // CHECK-MESSAGES: [[@LINE-1]]:10: warning: replace format string [fmt-printf-convert]
   // CHECK-FIXES: printf("Hello {} after\n", "Goodbye");
 }
+
+void printf_double() {
+  printf("Hello %f after\n", 42.0);
+  // CHECK-MESSAGES: [[@LINE-1]]:10: warning: replace format string [fmt-printf-convert]
+  // CHECK-FIXES: printf("Hello {} after\n", 42.0);
+}
+
+void printf_positional_arg() {
+  printf("Hello %2$d %1$s\n", "Goodbye", 42);
+  // CHECK-MESSAGES: [[@LINE-1]]:10: warning: replace format string [fmt-printf-convert]
+  // CHECK-FIXES: printf("Hello {2} {1}\n", "Goodbye", 42);
+}
+
+void printf_field_width() {
+  printf("Hello %3d after\n", 42);
+  // CHECK-MESSAGES: [[@LINE-1]]:10: warning: replace format string [fmt-printf-convert]
+  // CHECK-FIXES: printf("Hello {:3} after\n", 42);
+
+  printf("Hello %*d after\n", 4, 4242);
+  // CHECK-MESSAGES: [[@LINE-1]]:10: warning: replace format string [fmt-printf-convert]
+  // CHECK-FIXES: printf("Hello {:{}} after\n", 4, 4242);
+
+  printf("Hello %2$*1$d after\n", 5, 424242);
+  // CHECK-MESSAGES: [[@LINE-1]]:10: warning: replace format string [fmt-printf-convert]
+  // CHECK-FIXES: printf("Hello {2:{1}} after\n", 5, 424242);
+}
