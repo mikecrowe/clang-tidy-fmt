3c9331064ce7935e01a318655b963c0c2f093f5c
Mike Crowe <mac@mcrowe.com>
2021-04-03 16:44:00 +0100

    Reformat
    Fix/add file headers

--- clang-tools-extra/clang-tidy/fmt/FormatStringConverter.h
+++ clang-tools-extra/clang-tidy/fmt/FormatStringConverter.h
@@ -1,9 +1,17 @@
+//===--- FormatStringConverter.h - clang-tidy--------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
 #ifndef LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_FMT_FORMATSTRINGCONVERTER_H
 #define LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_FMT_FORMATSTRINGCONVERTER_H
 
-#include <string>
-#include "llvm/ADT/Optional.h"
 #include "clang/AST/ASTContext.h"
+#include "llvm/ADT/Optional.h"
+#include <string>
 
 namespace clang {
 namespace tidy {
--- clang-tools-extra/clang-tidy/fmt/PrintfConvertCheck.cpp
+++ clang-tools-extra/clang-tidy/fmt/PrintfConvertCheck.cpp
@@ -1,4 +1,4 @@
-//===--- PrintfConvertCheck.cpp - clang-tidy--------------------------------===//
+//===--- PrintfConvertCheck.cpp - clang-tidy---------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -7,8 +7,8 @@
 //===----------------------------------------------------------------------===//
 
 #include "PrintfConvertCheck.h"
-#include "clang/ASTMatchers/ASTMatchFinder.h"
 #include "FormatStringConverter.h"
+#include "clang/ASTMatchers/ASTMatchFinder.h"
 
 using namespace clang::ast_matchers;
 
@@ -17,36 +17,41 @@ namespace tidy {
 namespace fmt {
 
 void PrintfConvertCheck::registerMatchers(MatchFinder *Finder) {
-    StatementMatcher PrintfMatcher =
-        traverse(TK_AsIs,
-                 callExpr(callee(functionDecl(hasName("printf"))),
-                          hasArgument(0, stringLiteral().bind("format"))).bind("printf"));
-    Finder->addMatcher(PrintfMatcher, this);
+  StatementMatcher PrintfMatcher =
+      traverse(TK_AsIs, callExpr(callee(functionDecl(hasName("printf"))),
+                                 hasArgument(0, stringLiteral().bind("format")))
+                            .bind("printf"));
+  Finder->addMatcher(PrintfMatcher, this);
 
-    StatementMatcher FprintfMatcher =
-        traverse(TK_AsIs,
-                 callExpr(callee(functionDecl(hasName("fprintf"))),
-                          hasArgument(1, stringLiteral().bind("format"))).bind("printf"));
-    Finder->addMatcher(FprintfMatcher, this);
+  StatementMatcher FprintfMatcher =
+      traverse(TK_AsIs, callExpr(callee(functionDecl(hasName("fprintf"))),
+                                 hasArgument(1, stringLiteral().bind("format")))
+                            .bind("printf"));
+  Finder->addMatcher(FprintfMatcher, this);
 }
 
 void PrintfConvertCheck::check(const MatchFinder::MatchResult &Result) {
-  const auto *PrintfCall = Result.Nodes.getNodeAs<CallExpr>("printf")->getCallee();
+  const auto *PrintfCall =
+      Result.Nodes.getNodeAs<CallExpr>("printf")->getCallee();
   const auto *Format = Result.Nodes.getNodeAs<clang::StringLiteral>("format");
   const StringRef FormatString = Format->getString();
 
-  DiagnosticBuilder Diag = diag(PrintfCall->getBeginLoc(), "Replace printf with fmt::print");
-  Diag << FixItHint::CreateReplacement(CharSourceRange::getTokenRange(PrintfCall->getBeginLoc(),
-                                                                      PrintfCall->getEndLoc()),
-                                       "fmt::print");
+  DiagnosticBuilder Diag =
+      diag(PrintfCall->getBeginLoc(), "Replace printf with fmt::print");
+  Diag << FixItHint::CreateReplacement(
+      CharSourceRange::getTokenRange(PrintfCall->getBeginLoc(),
+                                     PrintfCall->getEndLoc()),
+      "fmt::print");
 
-  const auto MaybeReplacementFormatString = printfFormatStringToFmtString(Result.Context, FormatString);
+  const auto MaybeReplacementFormatString =
+      printfFormatStringToFmtString(Result.Context, FormatString);
   if (MaybeReplacementFormatString)
-    Diag << FixItHint::CreateReplacement(CharSourceRange::getTokenRange(Format->getBeginLoc(),
-                                                                        Format->getEndLoc()),
-                                         *MaybeReplacementFormatString);
+    Diag << FixItHint::CreateReplacement(
+        CharSourceRange::getTokenRange(Format->getBeginLoc(),
+                                       Format->getEndLoc()),
+        *MaybeReplacementFormatString);
 }
 
-}  // namespace fmt
-}  // namespace tidy
-}  // namespace clang
+} // namespace fmt
+} // namespace tidy
+} // namespace clang
