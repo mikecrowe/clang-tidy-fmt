ab31c879415d0c3d477cc7a2ceac14a028330597
Mike Crowe <mac@mcrowe.com>
2021-04-03 16:44:00 +0100

    Reformat
    Fix/add file headers

--- clang-tools-extra/clang-tidy/fmt/FormatStringConverter.cpp
+++ clang-tools-extra/clang-tidy/fmt/FormatStringConverter.cpp
@@ -1,3 +1,11 @@
+//===--- FormatStringConverter.cpp - clang-tidy------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
 #include "FormatStringConverter.h"
 #include "clang/AST/FormatString.h"
 #include "clang/Basic/LangOptions.h"
--- clang-tools-extra/clang-tidy/fmt/PrintfConvertCheck.h
+++ clang-tools-extra/clang-tidy/fmt/PrintfConvertCheck.h
@@ -1,4 +1,4 @@
-//===--- PrintfConvertCheck.h - clang-tidy------------------------*- C++ -*-===//
+//===--- PrintfConvertCheck.h - clang-tidy-----------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
